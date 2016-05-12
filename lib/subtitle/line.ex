7d07ea9e2b217c1761b3d83e8afe08b431a785b2
Jonatan da Hora <jonatandahora@gmail.com>
2016-05-12 19:07:14 -0300

    somewhat fixed line number parsing error and added timestamp converter function

--- lib/subtitle/line.ex
+++ lib/subtitle/line.ex
@@ -3,43 +3,57 @@ defmodule Parsex.Subtitle.Line do
   @type t :: %Parsex.Subtitle.Line{number: Integer.t, start_time: Float.t,
                                   end_time: Float.t, text: List.t}
 
-
   def parse_lines(file_stream) do
     Enum.chunk_by(file_stream, fn(line) -> line != "\n" end)
     |> Enum.filter(fn(elem) -> elem != ["\n"] end)
-    |> Enum.map(fn(line) -> Parsex.Subtitle.Line.parse_line(line) end)
+    |> Enum.map(fn(line) -> parse_line(line) end)
   end
 
-  def unescape_line(line) do
-    Enum.map(line, fn(text) ->
-      String.replace(text, ~r/\n|\t|\r/, "")
-    end)
-  end
-
-  def format_timestamp(timestamp) do
-    timestamp = String.split(timestamp, " --> ")
-    start_time = List.first(timestamp) |> timestamp_to_seconds
-    end_time = List.last(timestamp) |> timestamp_to_seconds
-    %{start_time: start_time, end_time: end_time}
-  end
-
-  defp timestamp_to_seconds(time) do
+  def timestamp_to_seconds(time) do
     [_, h, m, s, ms] = Regex.run(~r/(?<h>\d+):(?<m>\d+):(?<s>\d+)[,.](?<ms>\d+)/, time)
     (String.to_integer(h) * 3600)
     |> +((String.to_integer(m)) * 60)
     |> +(String.to_float("#{s}.#{ms}"))
   end
 
-  defp seconds_to_timestamp(seconds) do
-    
+  def seconds_to_timestamp(seconds) do
+    [sec_int, decimal] = Float.to_string(seconds, [decimals: 3])
+    |> String.split(".")
+    |> Enum.map(fn(elem) -> String.to_integer(elem) end)
+
+    h = round(Float.floor(seconds) / 3600) |> append_zero()
+    m = round(Float.floor(rem(sec_int, 3600) / 60)) |> append_zero()
+    s = rem(sec_int , 60) |> append_zero()
+    ms = decimal |> append_zero(:ms)
+
+    "#{h}:#{m}:#{s},#{ms}"
   end
 
-  def parse_line(line) do
+  defp unescape_line(line) do
+    Enum.map(line, fn(text) ->
+      String.replace(text, ~r/\n|\t|\r/, "")
+    end)
+  end
+
+  defp format_timestamp(timestamp) do
+    timestamp = String.split(timestamp, " --> ")
+    start_time = List.first(timestamp) |> timestamp_to_seconds
+    end_time = List.last(timestamp) |> timestamp_to_seconds
+    %{start_time: start_time, end_time: end_time}
+  end
+
+  defp append_zero(number, type \\ :normal) do
+    case type do
+      :ms ->
+        String.rjust(to_string(number), 3, ?0)
+      _ ->
+        String.rjust(to_string(number), 2, ?0)
+    end
+  end
+
+  defp parse_line(line) do
     line = unescape_line(line)
-    cond do
-      Enum.at(line, 0) == " " -> nil
-      Enum.at(line, 0) == "" -> nil
-      true ->
+    try do
         line_number = Enum.at(line, 0) |> String.to_integer()
         timestamp = Enum.at(line, 1) |> format_timestamp
         texts = Enum.drop(line, 2)
@@ -50,6 +64,8 @@ defmodule Parsex.Subtitle.Line do
           end_time: timestamp.end_time,
           text: texts
         }
+    rescue
+      _ -> nil
     end
   end
 end
